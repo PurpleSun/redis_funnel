67a4a1be1c7aca1309825dfeab0c51e8587b6294
fanwei.zeng <fanwei.zeng@cootek.cn>
2019-03-24 23:05:14 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: fanwei.zeng
+# Time: 2019/3/24 22:27
+from setuptools import setup, find_packages
+
+setup(
+    name="funnel",
+    url="TBD",
+    version="0.0.1",
+    keywords=("distributed", "funnel", "redis"),
+    description="A distributed funnel system based on redis, management system included.",
+    license="MIT License",
+    install_requires=["Flask==1.0.2", "redis==3.2.1"],
+    author="fanwei.zeng",
+    author_email="stayblank@gmail.com",
+    packages=find_packages(),
+    package_data={},
+    include_package_data=True,
+    platforms="any",
+)
